e25950100268a8093202c6e8513ae928a80cfbea
Allan Anderson <allana@shaw.ca>
2017-05-16 12:14:26 -0700

    day2

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+     public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int ArtistsId { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ReleaseLabel { get; set; }
+
+        //Navigation properties
+        public virtual ICollection<Track> Tracks { get; set; }
+        public virtual Artist Artist { get; set; }
+    }
+}
